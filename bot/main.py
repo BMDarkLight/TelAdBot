528980cda0dcf1f46b57b0689f1879cfa4bd7172
BMDarkLight <72068517+BMDarkLight@users.noreply.github.com>
2025-08-08 22:51:35 +0330

    feat: Error and Message Handling

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,8 @@ from telegram.ext import Application, ApplicationBuilder, CommandHandler, Messag
 
 import os
 
-# Explicitly load .env from the project root
 env_path = find_dotenv()
-print(f"Loading .env from: {env_path}")  # Debug: show where .env is loaded from
+print(f"Loading .env from: {env_path}")
 load_dotenv(env_path)
 
 TOKEN = os.environ.get("BOT_TOKEN")
@@ -50,6 +49,24 @@ async def check_membership_callback(update: Update, context: ContextTypes.DEFAUL
             reply_markup=reply_markup
         )
 
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message:
+        message_type: str = update.message.chat.type
+        text: str = update.message.text
+
+        print(f"--- Message ---\nReceived message: {text}\nChat type: {message_type}\nUser: {update.message.chat.id}")
+
+        response: str
+
+        if message_type == "private":
+            response = "پیام شما دریافت شد. در اسرع وقت پاسخ داده خواهد شد."
+
+        print(f"Bot: {response}")
+        await update.message.reply_text(response)
+        
+async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="یک مشکلی پیش آمد. لطفا دوباره تلاش کنید.")
 
 if __name__ == '__main__':
     print("Starting the bot ...")
@@ -61,6 +78,10 @@ if __name__ == '__main__':
         app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler('start', start_command))
+
+    app.add_handler(MessageHandler(filters.TEXT, message_handler))
+
+    app.add_error_handler(error_handler)
 
     print("Polling ...")
 
